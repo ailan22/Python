e1f2954c37249f451d4fe24569024ef0bc9f387f
Ailan Quintero <ailanquinteroh@gmail.com>
2021-08-11 13:52:40 -0300

    Clase 2

---
+++ ejercicios_python/Clase02/Camion.py
@@ -0,0 +1,9 @@
+#esfera.py
+#Ejercicio de esfera
+
+radio = float(input('Ingrese el radio de la esfera: '))
+volumen = 0
+
+volumen = 3.141592 * 4 / 3 * pow(radio, 3)
+
+print('El volumen de la esfera es: ', volumen)
